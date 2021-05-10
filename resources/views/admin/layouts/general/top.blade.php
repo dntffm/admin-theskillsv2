9facd155ba71b679329ef05b456eca1fb8e3f2d8
dntffm <dentamauuulana99@gmail.com>
2021-05-10 08:26:48 +0700

    stisla assets

--- resources/views/admin/layouts/general/top.blade.php
+++ resources/views/admin/layouts/general/top.blade.php
@@ -10,14 +10,14 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
   <!-- CSS Libraries -->
-  <link rel="stylesheet" href="{{asset('/stisla/node_modules/jqvmap/dist/jqvmap.min.css')}}">
-  <link rel="stylesheet" href="{{asset('/stisla/node_modules/weathericons/css/weather-icons.min.css')}}">
-  <link rel="stylesheet" href="{{asset('/stisla/node_modules/weathericons/css/weather-icons-wind.min.css')}}">
-  <link rel="stylesheet" href="{{asset('/stisla/node_modules/summernote/dist/summernote-bs4.css')}}">
+  <link rel="stylesheet" href="{{asset('/node_modules/jqvmap/dist/jqvmap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('/node_modules/weathericons/css/weather-icons.min.css')}}">
+  <link rel="stylesheet" href="{{asset('/node_modules/weathericons/css/weather-icons-wind.min.css')}}">
+  <link rel="stylesheet" href="{{asset('/node_modules/summernote/dist/summernote-bs4.css')}}">
 
   <!-- Template CSS -->
-  <link rel="stylesheet" href="{{asset('/stisla/assets/css/style.css')}}">
-  <link rel="stylesheet" href="{{asset('/stisla/assets/css/components.css')}}">
+  <link rel="stylesheet" href="{{asset('/assets/css/style.css')}}">
+  <link rel="stylesheet" href="{{asset('/assets/css/components.css')}}">
 </head>
 
 <body>
